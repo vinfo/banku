b128d73e8911ffb533678ea57617ab423f79f918
vinfo <victor.valencia@gmail.com>
2017-08-14 17:38:08 -0500

    varios

--- platforms/browser/www/assets/js/tools.js
+++ platforms/browser/www/assets/js/tools.js
@@ -1,5 +1,11 @@
 $( document ).ready(function(){    
     localStorage.setItem("site_url","http://localhost:6002");
+    var type_user= localStorage.type_user;
+    if(type_user=="prestatario"){
+        $(".inversionista").remove();
+    }else{
+        $(".prestatario").remove();
+    }
     $('select').material_select();
     $('.modal').modal();
     $( ".back" ).click(function() {
@@ -41,7 +47,7 @@ function spanishDate(d){
     return weekday[d.getDay()]+" "+d.getDate()+" de "+monthname[d.getMonth()]+" de "+d.getFullYear();
 }
 var getUserData= function getUserData(){
-    numeral.register('locale', 'fr', {
+    numeral.register('locale', 'es', {
         delimiters: {
             thousands: '.',
             decimal: ','
@@ -56,10 +62,10 @@ var getUserData= function getUserData(){
             return number === 1 ? 'er' : 'ème';
         },
         currency: {
-            symbol: '€'
+            symbol: '$'
         }
     });
-    numeral.locale('fr');    
+    numeral.locale('es');    
     if(localStorage.id_u!=""&&localStorage.type_user!=""){
         if(localStorage.type_user=="prestatario")$(".prestatario").show();
         $.ajax({
@@ -67,6 +73,9 @@ var getUserData= function getUserData(){
             url: "http://bankucolombia.com/lib/ajax_service_mobil.php",
             data: "action=getUserData&id_u="+localStorage.id_u+"&type_user="+localStorage.type_user,
             dataType:'JSON',
+            beforeSend: function( xhr ) {
+                $(".wrapper").hide();
+            },            
             success: function(msg){
                 //alert(JSON.stringify(msg));
                 if(msg.status==200&&msg.data){                    
@@ -81,9 +90,9 @@ var getUserData= function getUserData(){
                     $(".cellphone").html(msg.data.cellphone_u);
                     $(".address").html(msg.data.address_u);
                     var bank="";
-                    if(msg.data.banktype_u!="")bank+= msg.data.banktype_u;
-                    if(msg.data.bank_u!="")bank+= ", "+msg.data.bank_u;
-                    if(msg.data.account_u!="")bank+= " "+msg.data.account_u;
+                    if(msg.data.banktype_u!=""&&msg.data.banktype_u!=null)bank+= msg.data.banktype_u;
+                    if(msg.data.bank_u!=""&&msg.data.bank_u!=null)bank+= ", "+msg.data.bank_u;
+                    if(msg.data.account_u!=""&&msg.data.account_u!=null)bank+= " "+msg.data.account_u;
                     $(".bank").html(bank); 
                     var f= msg.data.date_2_u.split("-");
                     var lastLogin= new Date(f[0],f[1]-1,f[2]);
@@ -104,7 +113,9 @@ var getUserData= function getUserData(){
                     $(".star-user").html(starts);
                     $(".monto").html(numeral(msg.data.amount_u).format('0,0'));
                     $(".amount").html(numeral(msg.data.amount).format('0,0'));
-                    $(".days_study").html(msg.data.days_study);                    
+                    $(".days_study").html(msg.data.days_study);
+                    $(".wrapper").show();
+                    $(".progress").hide();                                        
                     console.log("Datos usuario");
                 }
             }
@@ -119,17 +130,21 @@ var getFullUserData= function getFullUserData(){
             url: "http://bankucolombia.com/lib/ajax_service_mobil.php",
             data: "action=getFullUserData&id_u="+localStorage.id_u,
             dataType:'JSON',
+            beforeSend: function( xhr ) {
+                $(".wrapper").hide();
+            },             
             success: function(msg){
                 if(msg.status==200&&msg.data){
-                    $.each(msg.data, function( index, value ) { 
+                    $.each(msg.data, function( index, value ) {                      
                       if(index=="photo_u"&&value!="")$(".photo").attr("src","data:image/jpeg;base64," + decodeURIComponent(value));
-                      if($("#"+index).length>0){
+                      if($("#"+index).length>0){                          
                           var text= $("#"+index).is('input:text');
+                          var tel= $("#"+index).is('[type=tel]');
                           var password= $("#"+index).is('input:password');
                           var select= $("#"+index).is('select');
-                          var checkbox= $("#"+index).is('input:checkbox');
+                          var checkbox= $("#"+index).is('input:checkbox');                         
                           
-                          if(text){
+                          if(text||tel){
                             $("#"+index).val(value);
                           }
                           if(password){
@@ -152,6 +167,8 @@ var getFullUserData= function getFullUserData(){
                     });
                     console.log("Datos usuario");
                 }
+                $(".wrapper").show();
+                $(".progress").hide();                 
             }
         });         
     }    
