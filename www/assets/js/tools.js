a0f1a3be6f907bf6aa9873b43c5cb6c34fd55fa9
VICTOR VALENCIA <victor.valencia@gmail.com>
2017-09-22 12:08:49 -0500

    cordova

--- www/assets/js/tools.js
+++ www/assets/js/tools.js
@@ -245,21 +245,6 @@ function getCalification(value){
         return 'AAA';
     }    
 }
-function send_notification(page,uuid){
-  alert("Notification");
-  //if(uuid==sessionStorage.UUID){
-    if(sessionStorage.UUID&&1==1){    
-      navigator.vibrate([0, 100, 100, 200, 50, 500]);
-      cordova.plugins.notification.local.schedule({
-        id: 1,
-        text: 'Nuevo mensaje negociación',
-        icon: 'http://bankucolombia.com/images/logoMobil.png',
-        sound: isAndroid ? 'file://beep.mp3' : 'file://beep.caf',
-        badge: 1,
-        data: { page: page }
-      });
-  }
-}
 var getUserData= function getUserData(){
     numeral.register('locale', 'es', {
         delimiters: {
--- www/index.js
+++ www/index.js
@@ -46,6 +46,21 @@ function setBadge(value){
     cordova.plugins.notification.badge;
     cordova.plugins.notification.badge.set(value);      
 }
+function send_notification(page,uuid){
+  alert("Notification");
+  //if(uuid==sessionStorage.UUID){
+    if(sessionStorage.UUID&&1==1){    
+      navigator.vibrate([0, 100, 100, 200, 50, 500]);
+      cordova.plugins.notification.local.schedule({
+        id: 1,
+        text: 'Nuevo mensaje negociación',
+        icon: 'http://bankucolombia.com/images/logoMobil.png',
+        sound: isAndroid ? 'file://beep.mp3' : 'file://beep.caf',
+        badge: 1,
+        data: { page: page }
+      });
+  }
+}
 function registerLog(log){
     console.log(log);
 }
