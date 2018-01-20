284034188b4675aab05bde72cb735aab5ddd5014
Vinfo <victor.valencia@gmail.com>
2018-01-20 01:51:45 -0500

    index

--- www/index.js
+++ www/index.js
@@ -1,61 +1,16 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
- var app = {
-    // Application Constructor
-    initialize: function() {
-      this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-      document.addEventListener('deviceready', this.onDeviceReady, false);
-      document.addEventListener("pause", this.onPause, false);
-      document.addEventListener("resume", this.onResume, false);      
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-      console.log("onDeviceReady");
-      getDeviceProperty();      
-      if(localStorage.OS&&localStorage.OS!="browser"){    
-        cordova.plugins.notification.badge;
-        checkConnection();
-      }
-    },
-    onPause: function() {
-      localStorage.setItem("login","true");
-      if(localStorage.OS&&localStorage.OS!="browser")cordova.plugins.notification.badge.clear();
-    },
-    onResume: function() {
-      if(localStorage.OS&&localStorage.OS!="browser"){
-          cordova.plugins.notification.badge.configure({ autoClear: true });
-          cordova.plugins.notification.badge.clear();
-        //console.log("Resumen");
-      }
-    }
-  };
+document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("pause", onPause, false);
+document.addEventListener("resume", onResume, false);
+
+function onDeviceReady() {    
+ getDeviceProperty();
+ checkConnection();    
+ if(localStorage.OS&&localStorage.OS!="browser"){    
+  cordova.plugins.notification.badge;
+}   
+}
 function getDeviceProperty() {
-    console.log("getDeviceProperty");
+    //console.log("getDeviceProperty");
     localStorage.setItem("OS", "browser");
     if(device){
       var deviceOS = device.platform;  //fetch the device operating system
@@ -64,9 +19,21 @@ function getDeviceProperty() {
       localStorage.setItem("OS", deviceOS);
       localStorage.setItem("UUID", uuid);
     }
-}
-function checkConnection() {
-    console.log("checkConnection");   
+  }
+  function onPause() {
+    localStorage.setItem("login","true");
+    if(localStorage.OS&&localStorage.OS!="browser")cordova.plugins.notification.badge.clear();
+      //console.log("Pausa");
+    }
+    function onResume() {
+      if(localStorage.OS&&localStorage.OS!="browser"){
+        cordova.plugins.notification.badge.configure({ autoClear: true });
+        cordova.plugins.notification.badge.clear();
+      //console.log("Resumen");
+    }
+  }  
+  function checkConnection() {
+    //console.log("checkConnection");
     var state = true;    
     if(localStorage.OS&&localStorage.OS!="browser"){        
       var networkState = navigator.connection.type;
@@ -86,8 +53,8 @@ function checkConnection() {
       }
     }
     return state;
-}
-function sendNotification(){
+  }
+  function sendNotification(){
     if(localStorage.OS&&localStorage.OS!="browser"){
       cordova.plugins.notification.badge.set(1);
       cordova.plugins.notification.local.schedule({
@@ -95,8 +62,5 @@ function sendNotification(){
         text: 'Existe una nueva oferta de negociacion...',
         foreground: true
       });    
-    }  
-}
-function registerLog(log){
-  //console.log(log);
-}
+    } 
+  }
