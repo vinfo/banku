4013e75797872278264a1344fcaca88b90f413c9
Vinfo <victor.valencia@gmail.com>
2018-01-22 00:20:11 -0500

    pause

--- www/index.js
+++ www/index.js
@@ -5,69 +5,73 @@ document.addEventListener("resume", onResume, false);
 function onDeviceReady() {    
  getDeviceProperty();
  checkConnection();    
- if(localStorage.OS&&localStorage.OS!="browser"){    
-  cordova.plugins.notification.badge;
-}   
+  if(localStorage.OS&&localStorage.OS!="browser"){    
+    cordova.plugins.notification.badge;
+  }   
 }
 function getDeviceProperty() {
-    //alert("getDeviceProperty");    
-    if(device){
-      var deviceOS = device.platform;  //fetch the device operating system
-      var deviceOSVersion = device.version;  //fetch the device OS version
-      var uuid = device.uuid;
-      localStorage.setItem("OS", deviceOS);
-      localStorage.setItem("UUID", uuid);
-    }
+  //alert("getDeviceProperty");    
+  if(device){
+    var deviceOS = device.platform;  //fetch the device operating system
+    var deviceOSVersion = device.version;  //fetch the device OS version
+    var uuid = device.uuid;
+    localStorage.setItem("OS", deviceOS);
+    localStorage.setItem("UUID", uuid);
   }
-  function onPause() {
-    localStorage.setItem("login","true");
-    if(localStorage.OS&&localStorage.OS!="browser")cordova.plugins.notification.badge.clear();
-      //console.log("Pausa");
-    }
-    function onResume() {
-      if(localStorage.OS&&localStorage.OS!="browser"){
-        cordova.plugins.notification.badge.configure({ autoClear: true });
-        cordova.plugins.notification.badge.clear();
-      //console.log("Resumen");
-    }
-  }  
-  function checkConnection() {
-    //alert("checkConnection");
-    var state = true;    
-    if(localStorage.OS&&localStorage.OS!="browser"){        
-      var networkState = navigator.connection.type;
-      var states = {};
-      states[Connection.UNKNOWN] = 'Unknown connection';
-      states[Connection.ETHERNET] = 'Ethernet connection';
-      states[Connection.WIFI] = 'WiFi connection';
-      states[Connection.CELL_2G] = 'Cell 2G connection';
-      states[Connection.CELL_3G] = 'Cell 3G connection';
-      states[Connection.CELL_4G] = 'Cell 4G connection';
-      states[Connection.CELL] = 'Cell generic connection';
-      states[Connection.NONE] = 'No network connection';
-      
-      if (states[networkState] == 'No network connection') {
-        alert('Problemas de conectividad a Internet!');
-        state = false;
-      }
-    }
-    return state;
-  }
-  function sendNotification(subject){
-    //alert("sendNotification");
-    navigator.vibrate(1200);
+}
+function onPause() {
+  localStorage.setItem("login","true");    
+  if(localStorage.OS&&localStorage.OS!="browser"){
+    localStorage.setItem("pause","true");
+    cordova.plugins.notification.badge.clear();
+  }      
+  //console.log("Pausa");
+}
+function onResume() {
     if(localStorage.OS&&localStorage.OS!="browser"){
-      //cordova.plugins.notification.badge.set(1);
-      cordova.plugins.notification.local.schedule({
-        title: subject,
-        sound: "file://beep.mp3",
-        icon: 'res://icon-36-ldpi.png',
-        smallIcon: 'res://icon-36-ldpi.png',
-        text: 'Existe una nueva oferta de negociacion...',
-        foreground: true
-      });    
-    } 
+      localStorage.removeItem("pause");
+      cordova.plugins.notification.badge.configure({ autoClear: true });
+      cordova.plugins.notification.badge.clear();
+    //console.log("Resumen");
   }
-  function registerLog(log){
-  //console.log(log);
+}  
+function checkConnection() {
+  //alert("checkConnection");
+  var state = true;    
+  if(localStorage.OS&&localStorage.OS!="browser"){        
+    var networkState = navigator.connection.type;
+    var states = {};
+    states[Connection.UNKNOWN] = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI] = 'WiFi connection';
+    states[Connection.CELL_2G] = 'Cell 2G connection';
+    states[Connection.CELL_3G] = 'Cell 3G connection';
+    states[Connection.CELL_4G] = 'Cell 4G connection';
+    states[Connection.CELL] = 'Cell generic connection';
+    states[Connection.NONE] = 'No network connection';
+    
+    if (states[networkState] == 'No network connection') {
+      alert('Problemas de conectividad a Internet!');
+      state = false;
+    }
+  }
+  return state;
+}
+function sendNotification(subject){
+  //alert("sendNotification");
+  navigator.vibrate(1200);
+  if(localStorage.OS&&localStorage.OS!="browser"&&localStorage.pause){
+    //cordova.plugins.notification.badge.set(1);
+    cordova.plugins.notification.local.schedule({
+      title: subject,
+      sound: "file://beep.mp3",
+      icon: 'res://icon-36-ldpi.png',
+      smallIcon: 'res://icon-36-ldpi.png',
+      text: 'Existe una nueva oferta de negociacion...',
+      foreground: true
+    });    
+  } 
+}
+function registerLog(log){
+//console.log(log);
 }
